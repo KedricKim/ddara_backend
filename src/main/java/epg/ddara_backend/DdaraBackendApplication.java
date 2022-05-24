c0088791047845c05e373cb358be53e080ee93b7
kedric <hgkim2@ntels.com>
2022-05-24 11:40:08 +0900

    first commit

---
+++ src/main/java/epg/ddara_backend/DdaraBackendApplication.java
@@ -0,0 +1,13 @@
+package epg.ddara_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdaraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdaraBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epg/ddara_backend/DdaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package epg.ddara_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdaraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
